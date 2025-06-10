9acb3b554190cda4af666b1488c31eb951ba4058
jonathan zollinger <jonathan.zollinger@gmail.com>
2025-06-10 15:58:15 -0600

    build: bump release to next snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("org.graalvm.buildtools.native") version "0.10.6"
 }
 
-version = "0.0.1"
+version = project.properties["justserve-cli-version"]!!
 group = "org.justserve"
 
 repositories {
