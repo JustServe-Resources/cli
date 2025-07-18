6644e444d2f8daf8cf64d1912efd2f21308582ac
jonathan zollinger <jonathan.zollinger@gmail.com>
2025-07-18 16:28:29 -0600

    refactor(tests): make tests more concise

--- src/test/groovy/org/justserve/client/UserClientSpec.groovy
+++ src/test/groovy/org/justserve/client/UserClientSpec.groovy
@@ -1,53 +1,22 @@
 package org.justserve.client
 
-import io.micronaut.context.ApplicationContext
+
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.exceptions.HttpClientResponseException
-import io.micronaut.runtime.server.EmbeddedServer
+import org.justserve.JustServeSpec
 import org.justserve.model.UserHashRequestByEmail
 import spock.lang.Shared
-import spock.lang.Specification
 
-class UserClientSpec extends Specification {
-
-    @Shared
-    ApplicationContext noAuthCtx
+class UserClientSpec extends JustServeSpec {
 
     @Shared
-    ApplicationContext ctx
+    UserClient noAuthUserClient, userClient
 
-    @Shared
-    UserClient noAuthUserClient
 
-    @Shared
-    UserClient userClient
-
-    void setupSpec() {
-        def justServeUrl = System.getenv("JUSTSERVE_URL") ?: "https://www.justserve.org"
-        if (null != System.getenv("JUSTSERVE_TOKEN")) {
-            throw new IllegalStateException("JUSTSERVE_TOKEN is set. Do not define this variable in testing.")
-        }
-        noAuthCtx = ApplicationContext.builder(EmbeddedServer)
-                .environmentVariableExcludes("JUSTSERVE_TOKEN", "TEST_TOKEN")
-                .properties(["micronaut.http.services.justserve.url": justServeUrl])
-                .build()
-                .start()
+    def setupSpec() {
         noAuthUserClient = noAuthCtx.getBean(UserClient)
-        ctx = ApplicationContext.builder(EmbeddedServer)
-                .environmentVariableExcludes("JUSTSERVE_TOKEN")
-                .properties([
-                        "justserve.token"                      : System.getenv("TEST_TOKEN"),
-                        "micronaut.http.services.justserve.url": justServeUrl
-                ])
-                .build()
-                .start()
         userClient = ctx.getBean(UserClient)
-    }
-
-    void cleanupSpec() {
-        noAuthCtx.stop()
-        ctx.stop()
     }
 
     def "get tempPassword for #email and "() {
@@ -77,9 +46,9 @@ class UserClientSpec extends Specification {
 
         where:
         expectedResponse | email                 | expectedException           | expectedMessage  | client           | _
-        HttpStatus.OK    | "jimmy@justserve.org" | null                        | null             | userClient       | _
+        HttpStatus.OK    | userEmail             | null                        | null             | userClient       | _
         null             | "notanemail@mail.moc" | HttpClientResponseException | "\"status\":500" | userClient       | _
-        null             | "jimmy@justserve.org" | HttpClientResponseException | "\"status\":401" | noAuthUserClient | _
+        null             | userEmail             | HttpClientResponseException | "\"status\":401" | noAuthUserClient | _
         null             | "notanemail@mail.moc" | HttpClientResponseException | "\"status\":401" | noAuthUserClient | _
     }
 }
